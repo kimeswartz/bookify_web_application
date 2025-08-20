739f8155636ff1f589b4bdca47675a54a44166dd
kimeswartz <kim.e.swartz@gmail.com>
2025-08-20 08:57:34 +0200

    lagt in maven och junit test i dependencies - test funkar

--- src/main/java/com/bookify/bookify_app/BookifyAppApplication.java
+++ src/main/java/com/bookify/bookify_app/BookifyAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookifyAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookifyAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookifyAppApplication.class, args);
+    }
 
 }
--- src/main/java/com/bookify/bookify_app/SecurityConfig.java
+++ src/main/java/com/bookify/bookify_app/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bookify.bookify_app;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,11 +12,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // stäng av CSRF (för enkel testning)
+                .csrf(AbstractHttpConfigurer::disable) // stäng av CSRF (för enkel testning)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // tillåt ALL trafik
                 );
         return http.build();
     }
+
 }
 
