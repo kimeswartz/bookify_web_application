b8376ddb94ffff4eadf54ae760dae982cb06f869
kimeswartz <kim.e.swartz@gmail.com>
2025-08-29 12:53:13 +0200

    removed test

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders hero heading from Tailwind v4 view', () => {
-    render(<App />);
-    expect(
-        screen.getByText(/Welcome to bookify built with Tailwind v4/i)
-    ).toBeInTheDocument();
-});
